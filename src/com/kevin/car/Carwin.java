9976f9c5a94b76dc356e97b74fe97932ad74b044
409570121 <ch11522914@gmail.com>
2021-06-01 15:26:05 +0800

    test1

--- src/com/kevin/car/Carwin.java
+++ src/com/kevin/car/Carwin.java
@@ -3,11 +3,12 @@ package com.kevin.car;
 import javax.swing.*;
 
 public class Carwin {
-    static  final int WIDTH = 600;
-    public static void main(String[] args){
+    static final int WIDTH = 600 ;
+    static final int HEIGHT=400 ;
+    public static void main(String[] args) {
         JFrame frame = new JFrame();
-        frame.setSize(WIDTH, 400);
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setSize(WIDTH,HEIGHT);
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         frame.setVisible(true);
     }
 }
