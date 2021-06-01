888e0a0ed63797b3e844770baac3c761f7eb1f4d
409570121 <ch11522914@gmail.com>
2021-06-01 16:43:13 +0800

    test1

---
+++ src/com/kevin/car/CarDemo.java
@@ -0,0 +1,20 @@
+package com.kevin.car;
+
+import  javax.swing.*;
+
+public class CarDemo {
+    private JPanel mainPanel;
+    private JTextField textField1;
+    private JTextField textField;
+    private  JButton button1;
+    private  JPanel northPanel;
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame();
+        frame.setContentPane(new CarDemo().mainPanel);
+        frame.setSize(600,400);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+}
